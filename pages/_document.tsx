08abafeacd7f38cfb1115866d011774a1201f0c6
Jan Wagner <info@janwagner-design.de>
2022-11-20 12:28:27 +0100

    added airbnb eslint config

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,7 +5,7 @@ const MyDocument: FunctionComponent = () => (
   <Html>
     <Head>
       <html lang='en' />
-      <link rel='icon' href='/favicon.ico'></link>
+      <link rel='icon' href='/favicon.ico' />
     </Head>
     <body>
       <Main />
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,19 +2,17 @@ import type { NextPage } from 'next'
 import { NextSeo } from 'next-seo'
 import React from 'react'
 
-const HomePage: NextPage = () => {
-  return (
-    <>
-      <NextSeo title='Next.js Starter' description='Bootstrap your next.js projects!' />
-      <main className='grid h-screen place-items-center'>
-        <section className='text-center'>
-          <h1>Next.js Starter Kit</h1>
-          <h2>Bootstrap your next projects!</h2>
-          <p>Typescript, TailwindCSS, sass and more...</p>
-        </section>
-      </main>
-    </>
-  )
-}
+const HomePage: NextPage = () => (
+  <>
+    <NextSeo title='Next.js Starter' description='Bootstrap your next.js projects!' />
+    <main className='grid h-screen place-items-center'>
+      <section className='text-center'>
+        <h1>Next.js Starter Kit</h1>
+        <h2>Bootstrap your next projects!</h2>
+        <p>Typescript, TailwindCSS, sass and more...</p>
+      </section>
+    </main>
+  </>
+)
 
 export default HomePage
