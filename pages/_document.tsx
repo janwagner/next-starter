2f245459eaaf76e24162c02b0c1f83eb8ede4288
Jan Wagner <jan.wagner@rio.cloud>
2024-01-14 08:43:16 +0100

    added responsive meta date to <head />

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,6 +4,8 @@ import { Head, Html, Main, NextScript } from 'next/document'
 const MyDocument: FunctionComponent = () => (
   <Html lang='en'>
     <Head>
+      <meta httpEquiv='X-UA-Compatible' content='IE=Edge' />
+      <meta name='viewport' content='initial-scale=1.0, width=device-width' />
       <link rel='icon' href='/favicon.ico' />
     </Head>
     <body>
