b47747c2dc00a65cf5b8611e3364292fd288fb8e
Jan <96049513+janwagner@users.noreply.github.com>
2024-02-18 08:12:53 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  reactStrictMode: false,
   swcMinify: true,
-  output: 'export',
+  // output: 'export',
 }
 
 module.exports = nextConfig
