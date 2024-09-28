05b1ed0edef16ac5f17207b947f90c62f3d77afa
Jan Wagner <info@janwagner-design.de>
2024-09-28 07:06:52 +0200

    biome "jsxQuoteStyle": "single"

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,7 +1,7 @@
 import { Head, Html, Main, NextScript } from "next/document";
 
 const App = () => (
-  <Html lang="en">
+  <Html lang='en'>
     <Head />
 
     <body>
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,8 +4,8 @@ import Seo from "../components/scaffolding/Seo";
 const IndexPage: NextPage = () => (
   <>
     <Seo />
-    <main className="grid h-screen place-items-center">
-      <section className="text-center">
+    <main className='grid h-screen place-items-center'>
+      <section className='text-center'>
         <h1>Next.js Starter Kit</h1>
         <h2>Bootstrap your next projects!</h2>
         <p>Typescript, TailwindCSS, sass and more...</p>
