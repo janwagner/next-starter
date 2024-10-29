466ca796ec9c57d99beb8319913ec54962464d90
Jan Wagner <info@janwagner-design.de>
2024-10-29 05:56:05 +0100

    updated sass settings

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
+  sassOptions: {
+    silenceDeprecations: ['legacy-js-api'],
+  },
 }
 
 module.exports = nextConfig
