cb9daa3363176ec19e2b8d2895fa0005e6fe9216
Jan Wagner <info@janwagner-design.de>
2025-03-17 06:26:05 +0100

    (update) migrated to nextjs app structure

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,11 +3,11 @@ import '@/styles/app.css'
 import type { ReactNode } from 'react'
 
 const Layout = ({ children }: { children: ReactNode }) => {
-  return (
-    <html lang='en'>
-      <body>{children}</body>
-    </html>
-  )
+    return (
+        <html lang='en'>
+            <body>{children}</body>
+        </html>
+    )
 }
 
 export default Layout
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
 const Page = () => (
-  <main className='grid h-screen place-items-center'>
-    <section className='text-center'>
-      <h1>Next.js Starter Kit</h1>
-      <h2>Bootstrap your next projects!</h2>
-      <p>Typescript, TailwindCSS, sass and more...</p>
-    </section>
-  </main>
+    <main className='grid h-screen place-items-center bg-gradient-to-br from-blue-500 via-sky-400 to-indigo-500'>
+        <section className='max-w-md text-balance rounded-xl bg-white bg-opacity-20 p-8 text-center shadow-lg backdrop-blur-md'>
+            <h1 className='mb-4 font-bold text-4xl'>Next.js Starter Kit</h1>
+            <h2 className='mb-6 font-semibold text-2xl'>Bootstrap your next projects!</h2>
+            <p className='text-lg leading-relaxed'>TypeScript, TailwindCSS, PostCSS, and more...</p>
+        </section>
+    </main>
 )
 
 export default Page
